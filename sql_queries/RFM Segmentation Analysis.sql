a11ca6d5924f15e7963b3a9aedcb2a6fb0a2af7f
Mohammad Ammar <theammarngp@gmail.com>
2026-03-30 23:11:42 +0530

    Update RFM Segmentation Analysis SQL query
    Refactored RFM segmentation SQL query for clarity and optimization.

--- sql_queries/RFM Segmentation Analysis.sql
+++ sql_queries/RFM Segmentation Analysis.sql
@@ -1,39 +1,4 @@
--- 	Q.	RFM Segmentation
--- For each customer:
--- 	Recency = days since last purchase
--- 	Frequency = total orders
--- 	Monetary = total revenue
-
-WITH  max_date as  (
-SELECT 
-      MAX(order_purchase_timestamp) as max_purchase_date
-      FROM orders 
-      WHERE order_status  != "canceled" 
-),
-rfm as 
-(SELECT 
-       c.customer_unique_id,
-       DATEDIFF( 
-       (SELECT max_purchase_date FROM max_date),
-       MAX(o.order_purchase_timestamp)
-       ) AS recency ,
- COUNT(DISTINCT o.order_id) as frequency,
-       SUM(oi.price + oi.freight_value) AS monetary
-        
-    FROM customers c
-    JOIN orders o
-        ON c.customer_id = o.customer_id
-    JOIN order_items oi
-        ON o.order_id = oi.order_id
-    WHERE o.order_status != 'canceled'
-    GROUP BY c.customer_unique_id
-)
-SELECT * FROM rfm 
-ORDER BY monetary DESC ;
-
-
 -- Q.RFM Segmentation (
-
 
 WITH max_date as(
 SELECT 
@@ -91,7 +56,6 @@ SELECT
 FROM rfm_segments
 GROUP BY customer_segment
 ORDER BY total_revenue DESC;
-
 
 -- The RFM analysis revealed that Champions represent a relatively small customer base (~13K) but generate the highest average revenue (306), indicating strong revenue concentration among top-tier customers.
 -- The Loyal segment forms the largest group (~41K) but shows lower average spending, suggesting upsell opportunities.
